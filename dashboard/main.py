d1c55ac4faa4ecb61666f04e859e8b89102a00e1
Sean Buxton <95533505+Noxx23@users.noreply.github.com>
2022-09-01 13:05:03 +0200

    Update main.py

--- dashboard/main.py
+++ dashboard/main.py
@@ -28,18 +28,10 @@ st.set_page_config(
 # -------------------------------------------
 # Everything is accessible via the st.secrets dict (subsection [config]) :
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
 st.write("My API_URL:", st.secrets["config"]["API_URL"])
 # And the root-level secrets are also accessible as environment variables:
 
-import os
-
-st.write(
-    "Has environment variables been set:",
-    os.environ["db_username"] == st.secrets["db_username"],
-)
 API_URL=st.secrets['config']['API_URL']
 default_threshold=st.secrets['config']['THRESHOLD']
 
